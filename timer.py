b5300e19e0d488f70e12f988cf8630d92f376d5e
jasontr <jasonacg@gmail.com>
2019-06-03 00:04:39 +0900

    make timer display seconds

--- timer.py
+++ timer.py
@@ -1,50 +1,86 @@
 from TimerUIElement import Ui_MainWindow
-from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtCore import QTime, QTimer, QDateTime
+from PyQt5.QtWidgets import QLCDNumber, QMainWindow, QApplication
 import sys
 
 
-class Timer(QtWidgets.QMainWindow, Ui_MainWindow):
+class Timer(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super(Timer, self).__init__()
         self.setupUi(self)
         self.additionalUISettings()
 
     def additionalUISettings(self):
-        self.learnTime
-        self.
         self.relaxPushButton.clicked.connect(self.relaxPushButtonClickedEvent)
         self.learnPushButton.clicked.connect(self.learnPushButtonClickedEvent)
-        self.learnLcd.setSegmentStyle(QtWidgets.QLCDNumber.Filled)
-        self.learnTimer = QtCore.QTimer(self.learnLcd)
-        self.learnTimer.timeout.connect(self.learnTimerShowTime)
-        self.learnTimer.started = False
-        self.learnTimer.start(53)
-
-        self.learnTimerShowTime()
-
-        self.setWindowTitle("Digital Clock")
+        self.learnTimer = QTimer(self.learnLcd)
+        self.relaxTimer = QTimer(self.relaxLcd)
+        self.learnTimeComponent = Time(self.learnTimer, self.learnLcd)
+        self.relaxTimeComponent = Time(self.relaxTimer, self.relaxLcd)
 
     def relaxPushButtonClickedEvent(self):
         print("greetings")
+        self.relaxTimeComponent.push()
 
     def learnPushButtonClickedEvent(self):
         print("hello")
-        if not self.learnTimer.started:
-            self.learnTimer.start(500)
+        self.learnTimeComponent.push()
 
     def learnTimerShowTime(self):
-        time = QtCore.QTime.currentTime()
+        pass
 
-        text = time.toString('hh:mm:ss:zzz')
-        if (time.second() % 2) == 0:
-            text = text[:2] + ' ' + text[3:5] + ' ' + text[6:8] + ' ' + text[9:]
+    def start(self):
+        pass
 
-        self.learnLcd.display(text)
+
+class Time:
+    def __init__(self, timer: QTimer, lcd: QLCDNumber):
+        self.timer = timer
+        self.lcd = lcd
+        self.lcd.setSegmentStyle(QLCDNumber.Filled)
+        # time variable
+        self.start_time = QDateTime.currentDateTime()
+        self.stop_time = QDateTime.currentDateTime()
+        self.curr_time = QDateTime(1, 1, 1, 0, 0)
+        self.timer.timeout.connect(self.currTime)
+        self.started = False
+
+    def currTime(self):
+
+        durationQDatetime, duration = self.calcDuration()
+        if not self.started:
+            durationQDatetime = self.curr_time
+        text = durationQDatetime.toString('hh:mm:ss.zzz')
+        # text = self.curr_time.toString('hh:mm:ss.zzz')
+        if (duration % 500) == 0:
+            text = text[:8] + ' ' + text[9:]
+        self.lcd.display(text)
+
+    def startTimer(self):
+        self.start_time = QDateTime.currentDateTime()
+        self.started = True
+        self.timer.start(53)
+
+    def stopTimer(self):
+        self.started = False
+        self.curr_time = self.calcDuration()[0]
+
+    def calcDuration(self) -> (QDateTime, int):
+        self.stop_time = QDateTime.currentDateTime()
+        duration = self.start_time.msecsTo(self.stop_time)
+        return self.curr_time.addMSecs(duration), duration
+
+
+    def push(self):
+        if self.started:
+            self.stopTimer()
+        else:
+            self.startTimer()
 
 
 if __name__ == "__main__":
     # Create a QApplication
-    app = QtWidgets.QApplication([])
+    app = QApplication([])
     widget = Timer()
     widget.show()
 
